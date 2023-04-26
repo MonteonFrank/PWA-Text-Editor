b0aa062b55d6ccdf12ce35fe67acb4e9b474aa98
Frank Monteon <frank.monteon@gmail.com>
2023-04-26 16:00:25 -0600

    Service Worker and Manifest working

---
+++ Develop/client/webpack.config.js
@@ -0,0 +1,66 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new MiniCssExtractPlugin(),
+      new WebpackPwaManifest({
+        name: 'Text Editor with PWA',
+        short_name: 'Text Editor',
+        description: 'Text Editor using PWA for Challenge 18',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: path.resolve(__dirname, './src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+        start_url:"./",
+        publicPath:"./",
+        orientation:"portrait"
+      }),
+
+      new InjectManifest({
+        swSrc: '/Users/frankmonteon/Challenges/Challenge 19 - PWA Text Editor/Text Editor/PWA-Text-Editor/Develop/client/src-sw.js', 
+        swDest: 'src-sw.js' 
+      })
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
+      ],
+    },
+  };
+};
